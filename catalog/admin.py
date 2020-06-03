ec9d58638d9fd484faf38d92d48d1e4af4f492bd
Hoda Elbadrawe <h.elbadrawe@gmail.com>
2020-06-03 23:36:54 +0200

    new veriosn with problems :"D

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, Profile
+from .models import User, Idcard
 
 # register user model
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('national_id', 'password')
 
-# register profile model
+@admin.register(Idcard)
+class IdcardAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'father_name')
+
+""" # register profile model
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('name', 'national_id', 'age', 'gender')
+    list_display = ('name', 'national_id', 'age', 'gender') """
--- catalog/models.py
+++ catalog/models.py
@@ -4,13 +4,48 @@ from django.db import models
 class User(models.Model):
 
     #fields
-    user_name = models.CharField(max_length=8, help_text='Enter username', primary_key=True)
+    national_id = models.BigIntegerField(help_text='Enter National ID', primary_key=True)
     password = models.CharField(max_length=30, help_text='Enter password')
 
     def __str__(self):
-        return self.user_name
+        return self.national_id
 
-class Profile(models.Model):
+class Idcard(models.Model):
+
+    religion_options = (
+        ('muslim', 'مسلم'),
+        ('Christian', 'مسيحي'),
+        ('Godless', 'بلا ديانة'),
+    )
+
+    gender_type = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    social_status_options = (
+        ('married', 'متزوج'),
+        ('Widowed', 'أرمل'),
+        ('divorced','مطلق'),
+        ('single', 'أعزب'),
+    )
+
+  
+    first_name = models.CharField(max_length=15, help_text='Enter First Name')
+    father_name = models.CharField(max_length=100, help_text='Enter Father Complete Name')
+    mother_name = models.CharField(max_length=100, help_text='Enter Mother Name')
+    religion = models.CharField(max_length=1, choices=religion_options)
+    gender = models.CharField(max_length=1, choices=gender_type)
+    social_status = models.CharField(max_length=50, choices=social_status_options)
+    governorate = models.CharField(max_length=15)
+    department = models.CharField(max_length=30)
+    village = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name
+
+
+""" class Profile(models.Model):
 
     gender_type = (
         ('M', 'Male'),
@@ -40,21 +75,15 @@ class Profile(models.Model):
     # محتاجة نضيف هنا خيارات بالحالة الاجتماعيه في البطاقة
     social_status = models.CharField(max_length=50, choices=social_status_options)
     job = models.CharField(max_length=50)
-    driving_license = models.CharField(max_length=1, choices=yes_or_no, help_text="add if thier driving license or not")
+    driving_license = models.CharField(max_length=1, choices=yes_or_no, help_text='add if thier driving license or not')
 
     def __str__(self):
-        return self.name
+        return self.name """
 
 """ class services(models.Model):
-
     service_name = models.CharField
-
 class birthregisteration(models.Model):
-
 class premiumservice(models.Model):
-
 class nationalidregesteration(models.Model):
-
 class drivinglicenseregisteration(models.Model):
-
 class payment(models.Model): """ 
